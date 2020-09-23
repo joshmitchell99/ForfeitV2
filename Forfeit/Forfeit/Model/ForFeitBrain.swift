8fa9ca8f036032d23b972c304518b4be360e1b45
Josh Mitchell <joshmit@stanford.edu>
2020-09-23 02:15:03 +0100

    Put all this stuff in the model. Tried to sort out the issue where it only displays one of the items and doesn't refresh, but I need a better look at the UITableView/go over Udemy. Hopefully you can sort this out as you know how it works.

--- Forfeit/Forfeit/Model/ForFeitBrain.swift
+++ Forfeit/Forfeit/Model/ForFeitBrain.swift
@@ -8,12 +8,58 @@
 
 import Foundation
 
-struct ForfeitBrain { 
+struct ForfeitBrain {
     
+    // Create a new array of item objects created from the item class
     var itemArray = [Item]()
+    
+    // Create a variable to hold the data URL of the filepath for the plist that contains the data to be saved
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    
     
     mutating func addForfeit(title: String, date: String, amount: Int) {
         
+        print("Adding this to The Brain: ", title, date, amount)
+        
+        let newItem = Item()
+        newItem.title = title
+        newItem.date = date
+        newItem.amount = amount
+        //self.itemArray.append(newItem)
+        
+        self.saveItem(item: newItem)
+        let forfeitListController = ForfeitListController()
+        forfeitListController.viewDidLoad()
     }
+    
+    mutating func saveItem(item: Item) {
+        print("Saving items")
+        let encoder = PropertyListEncoder()
+        
+        self.itemArray.append(item)
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            print("This is the data size", data)
+            try data.write(to: dataFilePath!) // Think this overwrites the original data. Hence we get the outcome we have on the app
+        } catch {
+            print("Error catching the encoded array, \(error)")
+        }
+    }
+    
+    mutating func loadItems() {
+        print("Loading items")
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                self.itemArray = try decoder.decode([Item].self, from: data)
+                print("array =", self.itemArray.count)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
+    
     
 }
